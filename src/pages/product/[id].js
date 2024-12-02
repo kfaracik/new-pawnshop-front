e63188d023948bd26acfb288103eac6a2c9c0de4
Krzysztof <larkfreeme70.55@gmail.com>
2024-12-02 13:32:42 +0100

    feat(api): added product api

--- src/pages/product/[id].js
+++ src/pages/product/[id].js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
-import axiosInstance from "lib/axiosInstance";
 import styled from "styled-components";
 import Link from "next/link";
 import Modal from "react-modal";
@@ -10,8 +9,8 @@ import Title from "components/Title";
 import WhiteBox from "components/WhiteBox";
 import Button from "components/Button";
 import CartIcon from "assets/icons/CartIcon";
+import { useProduct } from "services/api/useProductApi";
 
-// Styled Components
 const ColWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr;
@@ -101,101 +100,89 @@ const ProductPage = () => {
   const { query } = useRouter();
   const { id } = query;
 
-  const [product, setProduct] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const { data: product, isLoading } = useProduct(id);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    if (id) {
-      axiosInstance
-        .get(`/products/${id}`)
-        .then((response) => {
-          setProduct(response.data);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error("Error fetching product:", error);
-          setLoading(false);
-        });
-    }
-  }, [id]);
 
   const handleAddToCart = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
-  // if (loading) return <div>Loading...</div>;
-
-  if (!product) return <div>Product not found</div>;
-
   return (
-    <PageContainer loading={loading}>
-      <Center>
-        <ColWrapper>
-          <WhiteBox>
-            <ImagesWrapper>
-              {product.images.map((image, index) => (
-                <img
-                  key={index}
-                  src={image}
-                  alt={`${product.title} - ${index + 1}`}
-                />
-              ))}
-            </ImagesWrapper>
-          </WhiteBox>
-          <div>
-            <Chip primary>
-              <Link href="/contact">Produkt dostępny w naszym punkcie!</Link>
-            </Chip>
+    <PageContainer loading={isLoading}>
+      {!!product ? (
+        <>
+          <Center>
+            <ColWrapper>
+              <WhiteBox>
+                <ImagesWrapper>
+                  {product.images.map((image, index) => (
+                    <img
+                      key={index}
+                      src={image}
+                      alt={`${product.title} - ${index + 1}`}
+                    />
+                  ))}
+                </ImagesWrapper>
+              </WhiteBox>
+              <div>
+                <Chip primary>
+                  <Link href="/contact">
+                    Produkt dostępny w naszym punkcie!
+                  </Link>
+                </Chip>
 
-            <Title>{product.title}</Title>
-            <Price>{product.price.toFixed(2)} zł</Price>
-            <Description
-              dangerouslySetInnerHTML={{ __html: product.description }}
-            />
-            <Button
-              primary
-              onClick={handleAddToCart}
-              style={{
-                fontSize: "1.2rem",
-                padding: "15px 30px",
-              }}
-            >
-              <CartIcon />
-              Dodaj do koszyka
-            </Button>
-          </div>
-        </ColWrapper>
-      </Center>
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        style={{
-          content: {
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "rgba(255, 255, 255, 0.9)",
-            borderRadius: "12px",
-            padding: "20px",
-            maxWidth: "400px",
-            maxHeight: "400px",
-            margin: "auto",
-            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-          },
-          overlay: {
-            backgroundColor: "rgba(0, 0, 0, 0.75)",
-          },
-        }}
-      >
-        <ModalContent>
-          <h2>Produkt dostępny w naszym punkcie!</h2>
-          <p>
-            Ten produkt można odebrać tylko w naszym punkcie. Skontaktuj się z
-            nami, aby uzyskać więcej informacji.
-          </p>
-          <Link href="/contact">Przejdź do strony kontaktowej</Link>
-        </ModalContent>
-      </Modal>
+                <Title>{product.title}</Title>
+                <Price>{product.price.toFixed(2)} zł</Price>
+                <Description
+                  dangerouslySetInnerHTML={{ __html: product.description }}
+                />
+                <Button
+                  primary
+                  onClick={handleAddToCart}
+                  style={{
+                    fontSize: "1.2rem",
+                    padding: "15px 30px",
+                  }}
+                >
+                  <CartIcon />
+                  Dodaj do koszyka
+                </Button>
+              </div>
+            </ColWrapper>
+          </Center>
+          <Modal
+            isOpen={isModalOpen}
+            onRequestClose={closeModal}
+            style={{
+              content: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "rgba(255, 255, 255, 0.9)",
+                borderRadius: "12px",
+                padding: "20px",
+                maxWidth: "400px",
+                maxHeight: "400px",
+                margin: "auto",
+                boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+              },
+              overlay: {
+                backgroundColor: "rgba(0, 0, 0, 0.75)",
+              },
+            }}
+          >
+            <ModalContent>
+              <h2>Produkt dostępny w naszym punkcie!</h2>
+              <p>
+                Ten produkt można odebrać tylko w naszym punkcie. Skontaktuj się
+                z nami, aby uzyskać więcej informacji.
+              </p>
+              <Link href="/contact">Przejdź do strony kontaktowej</Link>
+            </ModalContent>
+          </Modal>
+        </>
+      ) : (
+        <div>Product not found</div>
+      )}
     </PageContainer>
   );
 };
