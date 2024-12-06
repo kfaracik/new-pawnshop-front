beb9e779054fe9c931da5b39c98a15eec1137e57
Krzysztof <larkfreeme70.55@gmail.com>
2024-12-06 10:22:41 +0100

    feat(scroll-indicator): added scroll indicator style

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,12 +2,20 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+
   body {
     background-color: #eee;
     padding: 0;
     margin: 0;
     font-family: 'Poppins', sans-serif;
+    overflow: hidden;
   }
+
+  #__next {
+    overflow-y: scroll;
+    height: 100vh;
+    scrollbar-width: thin;
+    scrollbar-color: #6c757d #dfe6e9;
 `;
 
 export default GlobalStyles;
