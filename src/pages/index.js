35bc2296f5a3038396d9eb6371e4ee8f512e2f1c
Krzysztof <larkfreeme70.55@gmail.com>
2024-12-18 00:52:44 +0100

    feat(home): added products lists

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,32 @@ import { useNewProducts } from "services/api/newProductsApi";
 import Slogan from "components/Slogan";
 
 export default function HomePage() {
-  const { data: newProducts, isLoading } = useNewProducts();
+  const { data: newProducts, isLoading: isLoadingNew } = useNewProducts();
+  const { data: popularProducts, isLoading: isLoadingPopular } =
+    useNewProducts();
+  const { data: suggestedProducts, isLoading: isLoadingSuggested } =
+    useNewProducts();
 
   return (
-    <PageContainer loading={isLoading}>
+    <PageContainer
+      loading={isLoadingNew || isLoadingPopular || isLoadingSuggested}
+    >
       <Slogan />
-      <NewProducts products={newProducts} />
+      <NewProducts
+        title="Nowości"
+        products={newProducts}
+        loading={isLoadingNew}
+      />
+      <NewProducts
+        title="Popularne"
+        products={popularProducts}
+        loading={isLoadingPopular}
+      />
+      <NewProducts
+        title="Sugerowane"
+        products={suggestedProducts}
+        loading={isLoadingSuggested}
+      />
     </PageContainer>
   );
 }
