8857514449926472bc5862f0dcd000a323829617
Krzysztof <larkfreeme70.55@gmail.com>
2024-12-29 23:45:43 +0100

    feat(home): add popular and suggested api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,17 @@
 import React from "react";
 import PageContainer from "components/PageContainer";
 import { useNewProducts } from "services/api/newProductsApi";
+import { usePopularProducts } from "services/api/popularProductsApi";
+import { useSuggestedProducts } from "services/api/suggestedProductsApi";
 import Slogan from "components/Slogan";
 import HorizontalProductList from "components/HorizontalProductList";
 
 export default function HomePage() {
   const { data: newProducts, isLoading: isLoadingNew } = useNewProducts();
   const { data: popularProducts, isLoading: isLoadingPopular } =
-    useNewProducts(); // todo: usePopularProducts();
+    usePopularProducts();
   const { data: suggestedProducts, isLoading: isLoadingSuggested } =
-    useNewProducts(); // todo: useSuggestedProducts();
-
-  // TODO: tmp function
-  const shuffleByField = (array, field, ascending = true) => {
-    if (!Array.isArray(array)) {
-      return [];
-    }
-
-    return [...array].sort((a, b) => {
-      const valueA = a[field];
-      const valueB = b[field];
-
-      if (ascending) {
-        return valueA > valueB ? 1 : valueA < valueB ? -1 : 0;
-      } else {
-        return valueB > valueA ? 1 : valueB < valueA ? -1 : 0;
-      }
-    });
-  };
-
-  const shuffledPopularProducts = popularProducts
-    ? shuffleByField(popularProducts, "title")
-    : [];
-  const shuffledSuggestedProducts = suggestedProducts
-    ? shuffleByField(suggestedProducts, "price")
-    : [];
+    useSuggestedProducts();
 
   return (
     <PageContainer
@@ -48,12 +25,12 @@ export default function HomePage() {
       />
       <HorizontalProductList
         title="Popularne"
-        products={shuffledPopularProducts}
+        products={popularProducts}
         loading={isLoadingPopular}
       />
       <HorizontalProductList
         title="Sugerowane"
-        products={shuffledSuggestedProducts}
+        products={suggestedProducts}
         loading={isLoadingSuggested}
       />
     </PageContainer>
